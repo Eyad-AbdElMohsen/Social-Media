4556633920fae26f131ed92c8475b5361ff0f186
EyadMohsen <ahmedeyad84@gmail.com>
2024-11-22 16:04:20 +0200

    allowedTo edit in js files

--- dist/middlewares/allowedTo.js
+++ dist/middlewares/allowedTo.js
@@ -7,17 +7,10 @@ exports.allowedTo = void 0;
 const api_error_1 = __importDefault(require("../errors/api.error"));
 const allowedTo = (roles) => {
     return (req, res, next) => {
-        if (req.currentUser) {
-            if (roles.includes(req.currentUser.role)) {
-                next();
-            }
-            else {
-                throw new api_error_1.default('You are not allowed to this action', 401);
-            }
-        }
-        else {
-            throw new Error('you should login');
-        }
+        if (roles.includes(req.currentUser.role))
+            next();
+        else
+            throw new api_error_1.default('You are not allowed to this action', 401);
     };
 };
 exports.allowedTo = allowedTo;
